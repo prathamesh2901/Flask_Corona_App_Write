19b6514bfc9d74b60be84363f22752c5752a6db4
Prathamesh Sawant <prathamesh2901@gmail.com>
2020-05-09 17:24:52 -0700

    Parametrized DB URI

--- app.py
+++ app.py
@@ -8,10 +8,17 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.country import Country
 from resources.state import State
+from os import environ
 
+DB_URL = environ["DB_URL"]
+DB_USER = environ["DB_USER"]
+DB_PASSWORD = environ["DB_PASSWORD"]
+DB_NAME = environ["DB_NAME"]
+
+DB_URI = 'mysql://{user}:{pw}@{url}/{db}'.format(user=DB_USER,pw=DB_PASSWORD,url=DB_URL,db=DB_NAME)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
